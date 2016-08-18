a5e7fe9c54e7c63e1d9151fe6cc7e19be1ecfa19
Remco <remco@dutchcoders.io>
2016-08-18 12:33:12 +0200

    fixed audit issues

--- Formula/elastico.rb
+++ Formula/elastico.rb
@@ -1,13 +1,12 @@
 class Elastico < Formula
+  desc "Command-line tool for Elasticsearch"
   homepage "https://github.com/dutchcoders/elastico"
   url "https://github.com/dutchcoders/elastico.git", :tag => "1.1", :revision => "4228ca86f7bf1e49e0359ddde126b78336de0ade"
-  version "1.1"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-
 
     clipath = buildpath/"src/github.com/dutchcoders/elastico"
     clipath.install Dir["*"]
